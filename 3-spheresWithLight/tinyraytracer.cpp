4ebe753636334680186463f53e26c5c5ce64be8a
zydiii <zydiiii@outlook.com>
2021-11-16 16:36:30 +0800

    add lights

--- 3-spheresWithLight/tinyraytracer.cpp
+++ 3-spheresWithLight/tinyraytracer.cpp
@@ -4,6 +4,13 @@
 #include <vector>
 #include <limits>
 
+struct Light
+{
+    Light(const vec3 &p, const float &i) : position(p), intensity(i) {}
+    vec3 position;
+    float intensity;
+};
+
 struct Material
 {
     Material(const vec3 &color) : diffuse_color(color) {}
@@ -54,7 +61,7 @@ bool scene_intersect(const vec3 &orig, const vec3 &dir, const std::vector<Sphere
     return spheres_dist < 1000;
 }
 
-vec3 cast_ray(const vec3 &orig, const vec3 &dir, const std::vector<Sphere> &spheres)
+vec3 cast_ray(const vec3 &orig, const vec3 &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights)
 {
     vec3 point, N;
     Material material;
@@ -64,10 +71,16 @@ vec3 cast_ray(const vec3 &orig, const vec3 &dir, const std::vector<Sphere> &sphe
         return vec3{0.2, 0.7, 0.8}; // background color
     }
 
-    return material.diffuse_color;
+    float diffuse_light_intensity = 0;
+    for (size_t i = 0; i < lights.size(); i++)
+    {
+        vec3 light_dir = (lights[i].position - point).normalize();
+        diffuse_light_intensity += lights[i].intensity * std::max(0.f, light_dir * N);
+    }
+    return material.diffuse_color * diffuse_light_intensity;
 }
 
-void render(const std::vector<Sphere> &spheres)
+void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights)
 {
     const int width = 1024;
     const int height = 768;
@@ -82,19 +95,19 @@ void render(const std::vector<Sphere> &spheres)
             float y = -(j + 0.5) + height / 2.;
             float z = -height / (2. * tan(fov / 2.));
             vec3 dir = vec3{x, y, z}.normalize();
-            framebuffer[i + j * width] = cast_ray(vec3{0, 0, 0}, dir, spheres);
+            framebuffer[i + j * width] = cast_ray(vec3{0, 0, 0}, dir, spheres, lights);
         }
     }
 
     std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./outSpheresImage.ppm", std::ios::binary);
+    ofs.open("./outSpheresWithLightsImage.ppm", std::ios::binary);
     ofs << "P6\n"
         << width << " " << height << "\n255\n";
     for (size_t i = 0; i < height * width; ++i)
     {
         for (size_t j = 0; j < 3; j++)
         {
-            ofs << (char)(255 * framebuffer[i][j]);
+            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
         }
     }
     ofs.close();
@@ -111,6 +124,9 @@ int main()
     spheres.push_back(Sphere(vec3{1.5, -0.5, -18}, 3, red_material));
     spheres.push_back(Sphere(vec3{7, 5, -18}, 4, purpel_material));
 
-    render(spheres);
+    std::vector<Light> lights;
+    lights.push_back(Light(vec3{-20, 20, 20}, 1.5));
+
+    render(spheres, lights);
     return 0;
 }
